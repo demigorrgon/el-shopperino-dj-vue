b4497e77c799697bc2d3fec215578c0452b7605f
demigorrgon <demigorrgon@gmail.com>
2022-05-07 15:15:25 +0300

    add api views

---
+++ shop/api/views.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets
+from shop.models import Product
+from shop.serializers import ProductSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import pagination
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    pagination_class = pagination.PageNumberPagination
+    page_size = 10
+
+    def get_object(self, queryset=None, **kwargs):
+        obj = self.kwargs.get("pk")
+        # print(self.kwargs)
+        return get_object_or_404(Product, slug=obj)
