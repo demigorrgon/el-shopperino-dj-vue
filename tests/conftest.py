d6ee0a46947d62bd8a3c415f8ec982cadbfefb20
demigorrgon <demigorrgon@gmail.com>
2022-05-07 12:17:28 +0300

    add test user

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from mixer.backend.django import mixer
+from rest_framework.test import APIClient
+from django.contrib.auth.models import User
 
 from shop.models import Category, Product
 
@@ -10,6 +12,17 @@ def dummy_user(django_user_model):
 
 
 @pytest.fixture
+def api_user_to_login():
+    return User.objects.create_user(
+        username="kekker1",
+        password="12345",
+        email="some@mail.com",
+        first_name="I",
+        last_name="wasda",
+    )
+
+
+@pytest.fixture
 def category():
     return mixer.blend(Category)
 
@@ -17,3 +30,9 @@ def category():
 @pytest.fixture
 def product():
     return mixer.blend(Product)
+
+
+@pytest.fixture
+def api():
+    client = APIClient()
+    return client
