591fd94266336828428da5df256a0e1c1a03d4d9
demigorrgon <demigorrgon@gmail.com>
2022-05-07 15:15:03 +0300

    upd urls

--- el_shopperino_core/urls.py
+++ el_shopperino_core/urls.py
@@ -23,7 +23,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    path("v1/api/", include("authapp.api.urls")),
+    path("api/v1/auth/", include("authapp.api.urls")),
+    path("api/v1/shop/", include("shop.api.urls")),
 ]
 
 if settings.DEBUG:
