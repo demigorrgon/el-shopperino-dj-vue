65bebfe446ebef95c0bb2d158de0ef5d28f2c9de
demigorrgon <demigorrgon@gmail.com>
2022-05-07 14:03:19 +0300

    upd settings

--- el_shopperino_core/settings.py
+++ el_shopperino_core/settings.py
@@ -161,7 +161,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_SCHEMA_CLASS": "rest_framework.schemas.coreapi.AutoSchema",
 }
 
 
