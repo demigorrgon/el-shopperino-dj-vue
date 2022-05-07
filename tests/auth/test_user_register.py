20d49628022a350c4b08a94318abb6c9e1fe9eae
demigorrgon <demigorrgon@gmail.com>
2022-05-07 12:19:56 +0300

    smol tests refactor

--- tests/auth/test_user_register.py
+++ tests/auth/test_user_register.py
@@ -17,28 +17,3 @@ def test_user_can_register(api):
         },
     )
     assert response.status_code == 201
-
-
-def test_user_retrieve_token_on_login(api, api_user_to_login):
-    url = reverse("token_obtain_pair")
-    response = api.post(
-        url,
-        {
-            "username": api_user_to_login.username,
-            "password": "12345",
-        },
-    )
-    print(api_user_to_login.username, api_user_to_login.password)
-    assert response.status_code == 200
-
-
-def test_token_returned_on_login(api, api_user_to_login):
-    url = reverse("token_obtain_pair")
-    response = api.post(
-        url,
-        {
-            "username": api_user_to_login.username,
-            "password": "12345",
-        },
-    )
-    assert response.data["access"]
