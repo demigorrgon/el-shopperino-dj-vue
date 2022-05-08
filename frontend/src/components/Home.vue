53468d7342458c39bb8aaf000279c25413dd8b7e
demigorrgon <demigorrgon@gmail.com>
2022-05-08 22:52:43 +0300

    upd components

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="root">
+    <p>wasda</p>
+    <p>token - {{ accessToken }}</p>
+    <button @click="verify">Verify</button>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+import { verifyToken } from "@/api/shortcuts";
+export default {
+  name: "Home",
+  data: () => {
+    return {};
+  },
+  computed: {
+    ...mapState(["accessToken", "refreshToken"]),
+  },
+  methods: {
+    async verify() {
+      await console.log(
+        verifyToken(this.accessToken).then((response) => {
+          console.log(response.status);
+        })
+      );
+    },
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="home">
+    <h1>Login</h1>
+    <br />
+    <form>
+      <vs-row>
+        <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12"
+          ><vs-input
+            class="inputx"
+            placeholder="Username"
+            style="margin-bottom: 10px"
+            v-model="username"
+          />
+        </vs-col>
+        <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12"
+          ><vs-input
+            class="inputx"
+            placeholder="Password"
+            style="margin-bottom: 10px"
+            type="password"
+            v-model="password"
+          />
+        </vs-col>
+        <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+          <vs-button color="dark" type="border" @click="login">Login</vs-button>
+          <!-- <button type="submit">Login</button> -->
+        </vs-col>
+      </vs-row>
+    </form>
+  </div>
+</template>
+
+<script>
+import { obtainToken } from "../api/shortcuts.js";
+import { mapMutations } from "vuex";
+export default {
+  data: () => {
+    return { username: "", password: "" };
+  },
+  methods: {
+    ...mapMutations(["setAccessToken", "setRefreshToken"]),
+    async login() {
+      const response = await obtainToken(this.username, this.password);
+      const accessToken = await response.data["access"];
+      const refreshToken = await response.data["refresh"];
+      this.setAccessToken(accessToken);
+      this.setRefreshToken(refreshToken);
+      this.$router.push("/");
+    },
+  },
+};
+</script>
+
+<style>
+</style>
