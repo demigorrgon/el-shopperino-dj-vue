474d344f7d9627add77dfcfd60d6afb33055b0c1
demigorrgon <demigorrgon@gmail.com>
2022-05-09 16:24:00 +0300

    refactor navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,51 +1,24 @@
 <template>
   <div id="app">
-    <!-- <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link>
-    </nav>
-    <router-view /> -->
-    <vs-navbar
-      v-model="indexActive"
-      color="black"
-      text-color="white"
-      active-text-color="white"
-      class="myNavbar"
-      style="border-radius: 0px 0px 20px 20px"
-    >
-      <div slot="title">
-        <vs-navbar-title style="margin-left: 100px">
-          <img src="@/assets/goblin-head.png" />
-        </vs-navbar-title>
-      </div>
-      <div class="item-wrapper">
-        <vs-navbar-item index="0">
-          <a href="#" v-if="activeUser === null">Home</a>
-          <a href="#" v-else>Some user</a>
-        </vs-navbar-item>
-        <vs-spacer></vs-spacer>
-        <vs-navbar-item index="1" v-if="activeUser === null">
-          <!-- <a href="#">News</a> -->
-          <router-link to="/login">Login</router-link>
-        </vs-navbar-item>
-        <vs-navbar-item index="2">
-          <a href="#">Cart</a>
-        </vs-navbar-item>
-      </div>
-    </vs-navbar>
+    <Navbar />
     <router-view />
   </div>
 </template>
 <script>
-import { mapGetters } from "vuex";
+import { mapGetters, mapState } from "vuex";
+import Navbar from "@/components/Navbar.vue";
 export default {
-  data: () => {
-    return {
-      // isLoggedIn: this.isLoggedIn,
-    };
+  components: {
+    Navbar,
+  },
+  created() {
+    this.$store.dispatch("isTokenValid");
+    if (this.$store.getters.tokenValid === false) {
+      this.$store.commit("logout");
+    }
   },
   methods: {},
-  computed: { ...mapGetters(["activeUser"]) },
+  computed: { ...mapGetters(["activeUser"]), ...mapState(["isTokenValid"]) },
 };
 </script>
 <style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+  <vs-navbar
+    color="black"
+    text-color="white"
+    active-text-color="white"
+    class="myNavbar"
+    style="border-radius: 0px 0px 20px 20px"
+  >
+    <div slot="title">
+      <vs-navbar-title style="margin-left: 100px">
+        <img src="@/assets/goblin-head.png" />
+      </vs-navbar-title>
+    </div>
+    <div class="item-wrapper">
+      <vs-navbar-item index="0">
+        <a href="#" v-if="activeUser === null">Home</a>
+        <a href="#" v-else><i>{{ this.$store.state.user }}</i></a>
+      </vs-navbar-item>
+      <vs-spacer></vs-spacer>
+      <vs-navbar-item index="1" v-if="activeUser === null">
+        <!-- <a href="#">News</a> -->
+        <router-link to="/login">Login</router-link>
+      </vs-navbar-item>
+      <vs-navbar-item index="2">
+        <a href="#">Cart</a>
+      </vs-navbar-item>
+    </div>
+  </vs-navbar>
+</template>
+
+<script>
+import { mapGetters, mapState, mapMutations } from "vuex";
+export default {
+  computed: {
+    ...mapGetters(["activeUser", "tokenValid"]),
+    ...mapState(["isTokenValid"]),
+  },
+  //   created() {
+  //     this.$store.dispatch("isTokenValid");
+  //     if (this.$store.getters.tokenValid === false) {
+  //       this.$store.commit("logout");
+  //     }
+  //   },
+  methods: {
+    ...mapMutations(["logout"]),
+  },
+};
+</script>
+
+<style>
+</style>
