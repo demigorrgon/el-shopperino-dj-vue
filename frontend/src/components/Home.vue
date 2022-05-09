876bf4400d7790528c959a733da0c2a3d8e0c690
demigorrgon <demigorrgon@gmail.com>
2022-05-09 16:24:11 +0300

    home cleanup

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,29 +1,25 @@
 <template>
   <div class="root">
     <p>wasda</p>
-    <p>token - {{ accessToken }}</p>
-    <button @click="verify">Verify</button>
+    <button @click="verify">verify jwt</button>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
-import { verifyToken } from "@/api/shortcuts";
+
 export default {
   name: "Home",
   data: () => {
     return {};
   },
   computed: {
-    ...mapState(["accessToken", "refreshToken"]),
+    ...mapState(["tokenValid"]),
   },
+
   methods: {
-    async verify() {
-      await console.log(
-        verifyToken(this.accessToken).then((response) => {
-          console.log(response.status);
-        })
-      );
+    verify() {
+      console.log(this.$store.dispatch("isTokenValid"));
     },
   },
 };
