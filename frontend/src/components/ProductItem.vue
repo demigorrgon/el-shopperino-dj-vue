6eb3506f58b93a69bbab823d07320079cca2caf4
demigorrgon <demigorrgon@gmail.com>
2022-05-09 22:20:35 +0300

    add product component

---
+++ frontend/src/components/ProductItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <vs-row>
+    <vs-col
+      vs-type="flex"
+      vs-justify="center"
+      vs-align="center"
+      vs-w="8"
+      vs-offset="2"
+      style="border: 1px solid gray"
+      >Products</vs-col
+    >
+  </vs-row>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
