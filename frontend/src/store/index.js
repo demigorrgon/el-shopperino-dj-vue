26141a414bf71c93db3131d7666317c5526d309b
demigorrgon <demigorrgon@gmail.com>
2022-05-09 16:25:00 +0300

    upd auth storage

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-// import { verifyToken } from '@/api/shortcuts'
+import { verifyToken } from '@/api/shortcuts'
+import jwt_decode from 'jwt-decode'
+import createPersistedState from 'vuex-persistedstate'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -8,16 +10,22 @@ export default new Vuex.Store({
     user: null,
     accessToken: null,
     refreshToken: null,
+    tokenValid: null
 
   },
   getters: {
-    isLoggedIn() {
-      return localStorage.getItem('accessToken') !== null // check if valid on top of it
+    activeUser(state) {
+      return state.user
+    },
+    tokenValid(state) {
+      return state.tokenValid
     }
   },
   mutations: {
-    authorizeUser(state, user) {
-      state.user = user;
+    authorizeUser(state) {
+      const decodedToken = jwt_decode(localStorage.getItem('accessToken'))
+      // check if valid on top of it
+      state.user = decodedToken.username
     },
     setAccessToken(state, token) {
       // state.accessToken = token;
@@ -26,23 +34,31 @@ export default new Vuex.Store({
 
     },
     setRefreshToken(state, token) {
-      console.log(token)
       localStorage.setItem('refreshToken', token)
       state.refreshToken = localStorage.getItem('refreshToken')
+    },
+    isTokenValid(state, isValid) {
+      state.tokenValid = isValid
+    },
+    logout(state) {
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('refreshToken')
+      state.user = null
+      state.accessToken = null
+      state.refreshToken = null
     }
   },
   actions: {
-    // initializeUser({ commit }) {
-    //   try {
-    //     let accessToken = localStorage.getItem('accessToken')
-    //     console.log(verifyToken(accessToken))
-    //     commit("authorizeUser", verifyToken(accessToken))
-    //   }
-    //   catch (error) {
-    //     console.log(error)
-    //   }
-    // }
+    isTokenValid({ commit }) {
+      return verifyToken(localStorage.getItem('accessToken')).then((response) => {
+        if (response.status === 200) {
+          commit('isTokenValid', true)
+        }
+      }).catch((err) => {
+        console.log(err.response.data.detail); commit('isTokenValid', false)
+      })
+
+    }
   },
-  modules: {
-  }
+  plugins: [createPersistedState()]
 })
