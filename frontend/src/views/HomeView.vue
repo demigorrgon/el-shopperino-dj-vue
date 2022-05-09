390eb62eeac053e07a9e89d3de7c43a6644f8f1f
demigorrgon <demigorrgon@gmail.com>
2022-05-09 22:20:58 +0300

    refactor views with components

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,14 +1,69 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
+  <div class="root">
+    <vs-row>
+      <vs-col
+        vs-type="flex"
+        vs-justify="center"
+        vs-align="center"
+        vs-w="6"
+        vs-offset="3"
+        class="relogin-modal"
+        v-if="isValid() === false"
+      >
+        <SessionExpired />
+      </vs-col>
+    </vs-row>
+    <p>wasda</p>
+    <button @click="verify">verify jwt</button>
+    <div class="cart" v-if="toggleCart === true">
+      <CartModal />
+    </div>
+    <br />
+    <button @click="toggleCartVisible">cart</button>
+    <br />
+    <br />
+    <br />
+    <ProductItem />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import CartModal from "../components/CartModal.vue";
+import SessionExpired from "../components/SessionExpired.vue";
+import ProductItem from "../components/ProductItem.vue";
+import { mapGetters } from "vuex";
 export default {
-  name: "HomeView",
-  components: {},
+  name: "Home",
+  components: {
+    CartModal,
+    SessionExpired,
+    ProductItem,
+  },
+  data: () => {
+    return {
+      toggleCart: false,
+    };
+  },
+
+  methods: {
+    verify() {
+      this.$store.dispatch("isTokenValid");
+      if (this.$store.getters.tokenValid === false) {
+        console.log("complain");
+      }
+    },
+    isValid() {
+      return this.$store.getters.tokenValid;
+    },
+    toggleCartVisible() {
+      this.toggleCart = !this.toggleCart;
+    },
+  },
+  computed: {
+    ...mapGetters(["isTokenValid"]),
+  },
 };
 </script>
+
+<style>
+</style>
