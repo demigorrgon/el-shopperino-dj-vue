4f43c409aa5bfcf74a80366f46fd982fa216a939
demigorrgon <demigorrgon@gmail.com>
2022-05-17 15:00:53 +0300

    add region select package

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,10 +5,13 @@ import store from './store'
 import Vuesax from 'vuesax'
 import 'vuesax/dist/vuesax.css'
 import 'material-icons/iconfont/material-icons.css';
+import vueCountryRegionSelect from 'vue-country-region-select'
+
 
 Vue.config.productionTip = false
 
 Vue.use(Vuesax)
+Vue.use(vueCountryRegionSelect)
 
 new Vue({
   router,
