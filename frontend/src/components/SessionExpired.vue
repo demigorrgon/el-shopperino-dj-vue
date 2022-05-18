a34687f6428ea6cb95d09103a7d3dcea0c85f1ab
demigorrgon <demigorrgon@gmail.com>
2022-05-18 22:51:50 +0300

    Upd token expired message so it actually logges user out

--- frontend/src/components/SessionExpired.vue
+++ frontend/src/components/SessionExpired.vue
@@ -1,5 +1,5 @@
 <template>
-  <vs-alert title="Danger" active="true" color="warning">
+  <vs-alert title="Danger" active="true" color="danger">
     Pls <router-link to="/login">relogin</router-link>, your session expired
   </vs-alert>
 </template>
@@ -9,6 +9,9 @@ import { mapGetters } from "vuex";
 export default {
   computed: {
     ...mapGetters(["tokenValid"]),
+  },
+  mounted() {
+    this.$store.commit("logout");
   },
 };
 </script>
