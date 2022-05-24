9a3f1cd58b8afe8527a3aac78c6c1db6d21b451e
demigorrgon <demigorrgon@gmail.com>
2022-05-24 21:09:32 +0300

    Update user model to CustomUser

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from profiles.models import Profile
 
-# Register your models here.
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ("user", "location", "bio", "created_at")
+    list_display = ("user", "created_at")
