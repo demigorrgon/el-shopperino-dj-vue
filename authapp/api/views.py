af4c3e84677f02f2e3e2f8e51baed3c9658a555d
demigorrgon <demigorrgon@gmail.com>
2022-05-24 21:05:33 +0300

    Add custom user instead of default django one

--- authapp/api/views.py
+++ authapp/api/views.py
@@ -1,19 +1,35 @@
 from rest_framework import viewsets
 from rest_framework import permissions
-from django.contrib.auth.models import User
+from authapp.models import CustomUser
+
+# from django.contrib.auth.models import User
 from rest_framework_simplejwt.views import TokenObtainPairView
 from authapp.serializers import UserCreateSerializer, UserSerializer, CustomObtainToken
+
+# from django.shortcuts import get_object_or_404
 
 
 class UserCreateViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.AllowAny]
-    queryset = User.objects.filter(is_active=True)
+    queryset = CustomUser.objects.filter(is_active=True)
+    pagination_class = None
 
     def get_serializer_class(self):
         if self.request.method == "GET":
             return UserSerializer
 
         return UserCreateSerializer
+
+    # def get_queryset(self):
+    #     qs = CustomUser.objects.all()
+    #     single_user_id = self.request.query_params.get("id")
+
+    #     if single_user_id:
+    #         qs = CustomUser.objects.filter(id=single_user_id)
+    # def get_object(self):
+    #     if self.kwargs.get("pk"):
+    #         obj = self.kwargs.get("pk")
+    #         return get_object_or_404(CustomUser, id=obj)
 
 
 class TokenObtainUsernameView(TokenObtainPairView):
--- authapp/serializers.py
+++ authapp/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from authapp.models import CustomUser
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from django_countries.serializers import CountryFieldMixin
 
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = User
+        model = CustomUser
         fields = (
             "username",
             "password",
@@ -17,19 +18,20 @@ class UserCreateSerializer(serializers.ModelSerializer):
         )
 
     def create(self, valid_data):
-        user = User.objects.create_user(
+        user = CustomUser.objects.create_user(
             username=valid_data["username"],
             password=valid_data["password"],
             email=valid_data["email"],
             first_name=valid_data["first_name"],
             last_name=valid_data["last_name"],
+            country=valid_data["country"],
         )
         return user
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(CountryFieldMixin, serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = CustomUser
         fields = [
             "username",
             "email",
@@ -37,7 +39,7 @@ class UserSerializer(serializers.ModelSerializer):
             "last_name",
             "is_staff",
             "is_active",
-            "date_joined",
+            "country",
         ]
 
 
