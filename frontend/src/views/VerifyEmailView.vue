1da1214db8192db230be760c1cc780574368f6a6
demigorrgon <demigorrgon@gmail.com>
2022-06-06 15:06:21 +0300

    Update views

--- frontend/src/views/VerifyEmailView.vue
+++ frontend/src/views/VerifyEmailView.vue
@@ -3,16 +3,37 @@
     <vs-row>
       <vs-col>
         <p style="margin-top: 20px">
-          We've sent you a message with email verification link. Pls activate
-          your account
+          {{ this.serverResponse }}
         </p>
+        <p>Redirecting in 3s...</p>
       </vs-col>
     </vs-row>
   </div>
 </template>
 
 <script>
-export default {};
+import { verifyEmailCode } from "../api/shortcuts";
+export default {
+  data() {
+    return {
+      codeValid: null,
+      serverResponse: "",
+    };
+  },
+  methods: {
+    verifyEmailCodeValid() {
+      verifyEmailCode(this.$route.params.emailCode)
+        .then((response) => (this.serverResponse = response.data.response))
+        .catch((e) => (this.serverResponse = e.response.data.response));
+
+      setTimeout(() => this.$router.push("/"), 3000);
+    },
+  },
+  mounted() {
+    // add vuex verification if mail was already sent b4 etc
+    this.verifyEmailCodeValid();
+  },
+};
 </script>
 
 <style>
