be8e9d5eb2c4a26862de14740e19569bfd700749
Valentyn Slivko <metallistvalon@gmail.com>
2022-06-16 23:28:33 +0300

    Add auto docs endpoints

--- el_shopperino_core/urls.py
+++ el_shopperino_core/urls.py
@@ -3,9 +3,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, re_path
-
-# from rest_framework.documentation import include_docs_urls
-# from rest_framework.schemas import get_schema_view
+from rest_framework.schemas import get_schema_view
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView,
@@ -16,15 +15,20 @@ urlpatterns = [
     path("api/token/", TokenObtainUsernameView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
-    # path("docs/", include_docs_urls(title="el-shopperinjoAPI")),
-    # path(
-    #     "schema",
-    #     get_schema_view(
-    #         title="el-shopperinjoAPI", description="api schema", version="1.0.0"
-    #     ),
-    #     name="coreapi_schema",
-    # ),
-    # path("v1/api/", include("authapp.api.urls")),
+    path(
+        "schema",
+        get_schema_view(
+            title="el-shopperinjoAPI", description="api schema", version="1.0.0"
+        ),
+        name="openapi_schema",
+    ),
+    path("api/docs/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/docs/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path("v1/api/", include("authapp.api.urls")),
     path("api/v1/auth/", include("authapp.api.urls")),
     path("api/v1/shop/", include("shop.api.urls")),
     re_path(r"api/v1/profiles?/", include("profiles.api.urls")),
